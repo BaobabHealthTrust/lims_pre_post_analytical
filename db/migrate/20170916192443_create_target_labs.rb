823dfe0e4e7b6b08522340d9ead4c18afec81b42
GiboProgrammer <giftmalolo@ymail.com>
2017-09-19 21:11:13 +0200

    added migration file for target labs table

--- db/migrate/20170916192443_create_target_labs.rb
+++ db/migrate/20170916192443_create_target_labs.rb
@@ -1,8 +1,11 @@
 class CreateTargetLabs < ActiveRecord::Migration[5.1]
   def change
     create_table :target_labs do |t|
-
+      t.string :name
+      t.string :district_id
+      t.string :phone_number
       t.timestamps
+      
     end
   end
 end
