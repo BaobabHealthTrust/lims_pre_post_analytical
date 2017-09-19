286d25a2cd397d08468631a9d7339cfad7a89c84
GiboProgrammer <giftmalolo@ymail.com>
2017-09-19 21:08:26 +0200

    allowing deletion and retrieving of labs

--- app/controllers/target_lab_controller.rb
+++ app/controllers/target_lab_controller.rb
@@ -1,7 +1,7 @@
 class TargetLabController < ApplicationController
 
 	def target_lab_main_page_loader_handler
-
+		@labs = TargetLab.get_target_labs
 	end
 
 
@@ -11,20 +11,25 @@ class TargetLabController < ApplicationController
 
 	def add_target_lab
 		lab_name = params[:lab][:name]
-		site_code = params[:lab][:site_code]
-		hosp = params[:lab][:hospital]
-		district = params[:lab][:district]
-		address = params[:lab][:address]
+		district = params[:district]		
 		phone = params[:lab][:phone]
-		if TargetLab.check_lab(lab_name,site_code,hosp,district,address,phone) == false
-			TargetLab.add_target_lab(lab_name,site_code,hosp,district,address,phone)
-			redirect_to '/ward_main_page_loader'
 
+		if TargetLab.check_lab(lab_name,district) == false
+			TargetLab.add_target_lab(lab_name,phone,district)
+			redirect_to '/target_lab_main_page_loader', flash: {message: 'target lab created successfuly'}
 		else
-			redirect_to '/ward_main_page_loader'
-
+			redirect_to '/add_target_lab_page_loader', flash: {error: 'target lab already exist in the system'}
 		end	
 	end
 
+	def delete_target_lab
+		lab_id = params[:lab_id]
+		TargetLab.delete_target_lab(lab_id)
+	end
+
+	def get_labs
+		labs = TargetLab.get_target_labs
+		render plain: labs.collect{|l| "<li>"+ l.name}.join("<li>")+"</li>"
+	end
 
 end
